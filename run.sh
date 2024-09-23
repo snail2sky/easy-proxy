0954f579c24b6ebe405beebbb84109d14cc9a0f1
snail <49757958+snail2sky@users.noreply.github.com>
2024-09-23 15:47:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+ssh_user=$1
+ssh_host=$2
+ssh_port=22
+ssh_tunnel_port=1081
+
 v2ray_url=
 v2ray_client_config_url=
 v2ray_server_config_url=
+
 
 get_v2ray(){
     wget $v2ray_url
@@ -15,4 +21,21 @@ get_v2ray_config(){
 
 modify_v2ray_config(){
     
+}
+
+gen_ssh_tunnel(){
+    ssh -NfR 127.0.0.1:$ssh_tunnel_port:127.0.0.1:$ssh_tunnel_port $ssh_user@$ssh_host
+}
+
+run_v2ray(){
+
+}
+
+main(){
+    get_v2ray
+    cd v2ray
+    get_v2ray_config
+    modify_v2ray_config
+    gen_ssh_tunnel
+    run_v2ray
 }
