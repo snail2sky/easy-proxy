16e67b7d456b5fe0ec46953890a5936b6990a856
snail <49757958+snail2sky@users.noreply.github.com>
2024-09-23 15:34:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+v2ray_url=
+v2ray_client_config_url=
+v2ray_server_config_url=
+
+get_v2ray(){
+    wget $v2ray_url
+}
+
+get_v2ray_config(){
+    wget $v2ray_client_config_url
+    wget $v2ray_server_config_url
+}
+
+modify_v2ray_config(){
+    
+}
